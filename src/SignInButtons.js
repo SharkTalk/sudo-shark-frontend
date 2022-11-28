5758b3e71b4a74aa35fb86914224d47f93825e26
Dan Pietsch <drpietsch14@gmail.com>
2022-11-28 11:58:13 -0500

    rebase merge

--- src/SignInButtons.js
+++ src/SignInButtons.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import React from "react";
+import { Button } from "@mui/material";
 
 function SignInButtons() {
-  return (
-    <div id='SignInButtons'>
-      <Button id='signin' variant='contained' color='secondary'>
-        Sign In
-      </Button>
-      <Button id='logout' color='secondary'>
-        Log Out
-      </Button>
-    </div>
-  );
+  function SignInButtons() {
+    return (
+      <div id="SignInButtons">
+        <Button id="signin" variant="contained" color="secondary">
+          Sign In
+        </Button>
+        <Button id="logout" color="secondary">
+          Log Out
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default SignInButtons;
