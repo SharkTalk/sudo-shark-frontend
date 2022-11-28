e5143d1b3a0beabe764d4b956726ed532cb973e7
Dan Pietsch <drpietsch14@gmail.com>
2022-11-28 12:01:47 -0500

    corrected file merge error

--- src/SignInButtons.js
+++ src/SignInButtons.js
@@ -1,21 +1,17 @@
-import React from "react";
-import { Button, TextField, Typography } from "@mui/material";
-import React from "react";
-import { Button } from "@mui/material";
+import React from 'react';
+import { Button } from '@mui/material';
 
 function SignInButtons() {
-  function SignInButtons() {
-    return (
-      <div id="SignInButtons">
-        <Button id="signin" variant="contained" color="secondary">
-          Sign In
-        </Button>
-        <Button id="logout" color="secondary">
-          Log Out
-        </Button>
-      </div>
-    );
-  }
+  return (
+    <div id='SignInButtons'>
+      <Button id='signin' variant='contained' color='secondary'>
+        Sign In
+      </Button>
+      <Button id='logout' color='secondary'>
+        Log Out
+      </Button>
+    </div>
+  );
 }
 
 export default SignInButtons;
