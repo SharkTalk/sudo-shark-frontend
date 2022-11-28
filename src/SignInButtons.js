5c1d709c82eea6106cc3d018e1a2996f762ffcb6
Dan Pietsch <drpietsch14@gmail.com>
2022-11-28 13:09:24 -0500

    login button creates dialog box that takes username and password

--- src/SignInButtons.js
+++ src/SignInButtons.js
@@ -1,12 +1,70 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import {
+  Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Box,
+} from '@mui/material';
 
 function SignInButtons() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    console.log('submitted', data.get('username'), data.get('password'));
+  };
+
   return (
     <div id='SignInButtons'>
-      <Button id='signin' variant='contained' color='secondary'>
+      <Button id='signin' variant='contained' color='secondary' onClick={handleClickOpen}>
         Sign In
       </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='username'
+              label='Username'
+              name='username'
+              autoComplete='off'
+              autoFocus
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='password'
+              label='Password'
+              name='password'
+              autoComplete='off'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+              color='secondary'
+            >
+              Sign In
+            </Button>
+          </Box>
+          {/* <form>
+            <label></label>
+            <input type='text' id='username' name='username' />
+          </form> */}
+
+        </DialogContent>
+      </Dialog>
       <Button id='logout' color='secondary'>
         Log Out
       </Button>
